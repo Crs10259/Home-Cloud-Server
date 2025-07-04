c1847eb7f7bdd8fee607947a70f2033c653f5d8d
Crs102597 <961521953@qq.com>
2025-07-04 21:18:15 +0800

    Update

--- init_db.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-from app.extensions import db
-from app.models.activity import Activity
-
-app = create_app()
-with app.app_context():
-    db.create_all()
-    print("Database initialized successfully") 
